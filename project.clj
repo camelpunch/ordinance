a11599cd2324309dccc1e9b412b970504ace9c6e
Andrew Bruce <andrew@camelpunch.com>
2015-05-09 15:22:23 +0100

    Description and URL in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.camelpunch/ordinance "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Convert Ordinance Survey coordinates to latitude/longitude"
+  :url "https://github.com/camelpunch/ordinance"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["array systems" {:url "http://corp.array.ca/nest-web/maven/"
